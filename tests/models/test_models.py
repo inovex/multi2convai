aa8007c89e9179d8b6506b5c6533f1e71493bfab
Sebastian Blank <33516140+sebastianblank@users.noreply.github.com>
2022-01-21 14:50:41 +0100

    feat: add logistic regression (#4)
    * Add logistic regression
    
    * Fix import
    
    Co-authored-by: Sebastian Blank <sbstn.blank@gmail.com>

---
+++ tests/models/test_models.py
@@ -0,0 +1,26 @@
+from torch.nn import Linear, Module
+
+from multi2convai.models.logistic_regression import LogisticRegression
+
+
+class TestLogisticRegression:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    def test_create_object(self):
+        # Given
+        input_dim, output_dim = 4, 2
+
+        # When
+        test_result = LogisticRegression(input_dim, output_dim)
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, LogisticRegression)
+        assert isinstance(test_result, Module)
+        assert isinstance(test_result.linear, Linear)
+        assert test_result.linear.in_features == input_dim
+        assert test_result.linear.out_features == output_dim
