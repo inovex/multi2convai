1362a27b9d30a555a8be14c4b5f4231618d88f96
Sebastian Blank <33516140+sebastianblank@users.noreply.github.com>
2022-01-21 14:37:26 +0100

    feat: add preprocessor (#2)
    * Add preprocessor
    
    * Fix import
    
    * Add inits
    
    Co-authored-by: Sebastian Blank <sbstn.blank@gmail.com>

---
+++ src/multi2convai/preprocessing/preprocessor.py
@@ -0,0 +1,94 @@
+import re
+import string
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class PreprocessorStep(ABC):
+    """Abstract base class for preprocessing steps."""
+
+    @abstractmethod
+    def execute(self, text: str) -> str:
+        raise NotImplementedError
+
+
+class ReplaceWhitespaces(PreprocessorStep):
+    """Prepressing step which replaces multiple subsequent whitespaces by a single one."""
+
+    def execute(self, text: str) -> str:
+        return re.sub(r"\s+", " ", text)
+
+
+class ReplaceNumerics(PreprocessorStep):
+    """Preprocessing step which replaces digits in a given string by zeros."""
+
+    def execute(self, text: str) -> str:
+        return re.sub(r"\d+", "0", text)
+
+
+class Lowercase(PreprocessorStep):
+    """Preprocessing step which lowercases the given text."""
+
+    def execute(self, text: str) -> str:
+        return text.lower()
+
+
+class RemovePunctuation(PreprocessorStep):
+    """Preprocessing step which removes punctuation from a given text."""
+
+    def __init__(self):
+        self.translator = str.maketrans("", "", string.punctuation)
+
+    def execute(self, text: str) -> str:
+        text = text.translate(self.translator)
+        text = text.replace("=", " ")
+        text = text.replace("%", " ")
+
+        return text
+
+
+class Preprocessor:
+    """Preprocessor applies a series of standard string manipulations on a given list of texts. Per default, the
+    preprocessor replaces multiple subsequent whitespaces, lowercases, replaces digits with zeros and removes
+    punctuation from the given texts.
+
+    Args:
+        lower (bool): will lowercase given texts during preprocessing if True.
+        replace_digits (bool): will replace digits with zero during preprocessing if True.
+        remove_punctuation (bool): will remove punctuations during preprocessing if True.
+    """
+
+    def __init__(
+        self,
+        lower: bool = True,
+        replace_digits: bool = True,
+        remove_punctuation: bool = True,
+    ):
+        self.steps = [ReplaceWhitespaces()]
+
+        if lower:
+            self.steps.append(Lowercase())
+
+        if replace_digits:
+            self.steps.append(ReplaceNumerics())
+
+        if remove_punctuation:
+            self.steps.append(RemovePunctuation())
+
+    def preprocess(self, texts: List[str]) -> List[str]:
+        """Applies preprocessing to a given list of text.
+
+        Args:
+            text (List[str]): list of text to be preprocessed
+
+        Returns:
+            List[str]: preprocessed texts
+        """
+        result = []
+
+        for text in texts:
+            for step in self.steps:
+                text = step.execute(text)
+            result.append(text)
+
+        return result
---
+++ tests/preprocessing/test_preprocessor.py
@@ -0,0 +1,254 @@
+from typing import List
+
+import pytest
+
+from multi2convai.preprocessing.preprocessor import (
+    Lowercase,
+    Preprocessor,
+    PreprocessorStep,
+    RemovePunctuation,
+    ReplaceNumerics,
+    ReplaceWhitespaces,
+)
+
+
+class TestReplaceWhitespaces:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    def test_create_object(self):
+        # Given
+
+        # When
+        test_result = ReplaceWhitespaces()
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, ReplaceWhitespaces)
+        assert isinstance(test_result, PreprocessorStep)
+
+    def test_execute(self):
+        # Given
+        step = ReplaceWhitespaces()
+        texts = "This is a    text with whitespaces.\n\nthe end."
+
+        # When
+        test_result = step.execute(texts)
+
+        # Then
+        assert test_result is not None
+        assert test_result == "This is a text with whitespaces. the end."
+
+
+class TestReplaceNumerics:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    def test_create_object(self):
+        # Given
+
+        # When
+        test_result = ReplaceNumerics()
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, ReplaceNumerics)
+        assert isinstance(test_result, PreprocessorStep)
+
+    def test_execute(self):
+        # Given
+        step = ReplaceNumerics()
+        texts = "This is a text with 12345 numbers."
+
+        # When
+        test_result = step.execute(texts)
+
+        # Then
+        assert test_result is not None
+        assert test_result == "This is a text with 0 numbers."
+
+
+class TestRemovePunctuation:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    def test_create_object(self):
+        # Given
+
+        # When
+        test_result = RemovePunctuation()
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, RemovePunctuation)
+        assert isinstance(test_result, PreprocessorStep)
+
+    def test_execute(self):
+        # Given
+        step = RemovePunctuation()
+        texts = "This is a text with no comma, and no period."
+
+        # When
+        test_result = step.execute(texts)
+
+        # Then
+        assert test_result is not None
+        assert test_result == "This is a text with no comma and no period"
+
+
+class TestLowercase:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    def test_create_object(self):
+        # Given
+
+        # When
+        test_result = Lowercase()
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, Lowercase)
+        assert isinstance(test_result, PreprocessorStep)
+
+    def test_execute(self):
+        # Given
+        step = Lowercase()
+        texts = "This is a text in LOWERCASE."
+
+        # When
+        test_result = step.execute(texts)
+
+        # Then
+        assert test_result is not None
+        assert test_result == "this is a text in lowercase."
+
+
+class TestPreprocessor:
+    def setup(self):
+        pass
+
+    def setup_class(self):
+        pass
+
+    @pytest.mark.parametrize(
+        ["lower", "replace_digits", "remove_punctuation", "expected_steps"],
+        [
+            [
+                True,
+                True,
+                True,
+                [ReplaceWhitespaces, Lowercase, ReplaceNumerics, RemovePunctuation],
+            ],
+            [False, False, False, [ReplaceWhitespaces]],
+            [
+                False,
+                True,
+                True,
+                [ReplaceWhitespaces, ReplaceNumerics, RemovePunctuation],
+            ],
+        ],
+    )
+    def test_create_object(
+        self,
+        lower: bool,
+        replace_digits: bool,
+        remove_punctuation: bool,
+        expected_steps: List,
+    ):
+        # Given
+
+        # When
+        test_result = Preprocessor(
+            lower=lower,
+            replace_digits=replace_digits,
+            remove_punctuation=remove_punctuation,
+        )
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, Preprocessor)
+        assert isinstance(test_result.steps, list)
+        assert len(test_result.steps) == len(expected_steps)
+        for step, expected_step in zip(test_result.steps, expected_steps):
+            assert isinstance(step, expected_step)
+
+    def test_create_object_no_args(self):
+        # Given
+        expected_steps = [
+            ReplaceWhitespaces,
+            Lowercase,
+            ReplaceNumerics,
+            RemovePunctuation,
+        ]
+
+        # When
+        test_result = Preprocessor()
+
+        # Then
+        assert test_result is not None
+        assert isinstance(test_result, Preprocessor)
+        assert isinstance(test_result.steps, list)
+        assert len(test_result.steps) == len(expected_steps)
+        for step, expected_step in zip(test_result.steps, expected_steps):
+            assert isinstance(step, expected_step)
+
+    @pytest.mark.parametrize(
+        ["lower", "replace_digits", "remove_punctuation", "expected_results"],
+        [
+            [
+                False,
+                False,
+                False,
+                [
+                    "This is a test with 012 numbers in 3.",
+                    "Another text with 33 counts!",
+                ],
+            ],
+            [
+                True,
+                True,
+                True,
+                ["this is a test with 0 numbers in 0", "another text with 0 counts"],
+            ],
+        ],
+    )
+    def test_preprocess(
+        self,
+        lower: bool,
+        replace_digits: bool,
+        remove_punctuation: bool,
+        expected_results: List[str],
+    ):
+        # Given
+        texts = [
+            "This is a test with 012 numbers in 3.",
+            "Another text with\n\n 33 counts!",
+        ]
+        preprocessor = Preprocessor(
+            lower=lower,
+            replace_digits=replace_digits,
+            remove_punctuation=remove_punctuation,
+        )
+
+        # When
+        test_results = preprocessor.preprocess(texts)
+
+        # Then
+        assert test_results is not None
+        assert isinstance(test_results, list)
+        assert len(test_results) == len(expected_results)
+        for test_result, expected_result in zip(test_results, expected_results):
+            assert test_result == expected_result
